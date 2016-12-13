aa2b9f8e3452124efc6f495a7e3cbca076098a8f
Nick Miller <nick.miller@onyxpoint.com>
2016-12-13 10:40:22 -0500

    (SIMP-2231) Create timezone module
    Timezone used to be managed in simplib.
    
    Updated saz/timezone for Puppet 4, including strongly typed modules and
    data in modules.
    
    SIMP-2231 #close
    SIMP-1679 #comment created timezone module

--- spec/support/gentoo.rb
+++ spec/support/gentoo.rb
@@ -1,24 +1,31 @@
 shared_examples 'Gentoo' do
-  let(:facts) {{ :osfamily => "Gentoo" }}
+  let(:facts) {{
+    :os => {
+      :family => 'Gentoo',
+      :release => {
+        :full => '4.8.0',
+        :major => '4'
+      }
+    }
+  }}
 
   describe "when using default class parameters" do
     let(:params) {{ }}
 
-    it { should create_class('timezone') }
-    it { should contain_class('timezone::params') }
+    it { is_expected.to create_class('timezone') }
 
     it do
-      should contain_package('sys-libs/timezone-data').with({
+      is_expected.to contain_package('sys-libs/timezone-data').with({
         :ensure => 'present',
         :before => 'File[/etc/localtime]',
       })
     end
 
-    it { should contain_file('/etc/timezone').with_ensure('file') }
-    it { should contain_file('/etc/timezone').with_content(/^Etc\/UTC$/) }
-    it { should contain_exec('update_timezone').with_command(/^emerge --config timezone-data$/) }
+    it { is_expected.to contain_file('/etc/timezone').with_ensure('file') }
+    it { is_expected.to contain_file('/etc/timezone').with_content(/^Etc\/UTC$/) }
+    it { is_expected.to contain_exec('update_timezone').with_command(/^emerge --config timezone-data$/) }
     it do
-      should contain_file('/etc/localtime').with({
+      is_expected.to contain_file('/etc/localtime').with({
         :ensure => 'file',
         :source => 'file:///usr/share/zoneinfo/Etc/UTC',
       })
@@ -27,20 +34,20 @@ shared_examples 'Gentoo' do
     context 'when timezone => "Europe/Berlin"' do
       let(:params) {{ :timezone => "Europe/Berlin" }}
 
-      it { should contain_file('/etc/timezone').with_content(/^Europe\/Berlin$/) }
-      it { should contain_file('/etc/localtime').with_source('file:///usr/share/zoneinfo/Europe/Berlin') }
+      it { is_expected.to contain_file('/etc/timezone').with_content(/^Europe\/Berlin$/) }
+      it { is_expected.to contain_file('/etc/localtime').with_source('file:///usr/share/zoneinfo/Europe/Berlin') }
     end
 
     context 'when autoupgrade => true' do
       let(:params) {{ :autoupgrade => true }}
-      it { should contain_package('sys-libs/timezone-data').with_ensure('latest') }
+      it { is_expected.to contain_package('sys-libs/timezone-data').with_ensure('latest') }
     end
 
     context 'when ensure => absent' do
       let(:params) {{ :ensure => 'absent' }}
-      it { should contain_package('sys-libs/timezone-data').with_ensure('present') }
-      it { should contain_file('/etc/timezone').with_ensure('absent') }
-      it { should contain_file('/etc/localtime').with_ensure('absent') }
+      it { is_expected.to contain_package('sys-libs/timezone-data').with_ensure('present') }
+      it { is_expected.to contain_file('/etc/timezone').with_ensure('absent') }
+      it { is_expected.to contain_file('/etc/localtime').with_ensure('absent') }
     end
 
     include_examples 'validate parameters'
--- spec/support/validate_params.rb
+++ spec/support/validate_params.rb
@@ -3,8 +3,15 @@ shared_examples_for 'validate parameters' do
     'autoupgrade',
   ].each do |param|
     context "with #{param} => 'foo'" do
+      let(:facts) {{
+        :os => {
+          :name => 'Debian',
+          :family => 'Debian',
+          :release => { :major => 8, :full => 8 }
+        }
+      }}
       let(:params) {{ param.to_sym => 'foo' }}
-      it { expect { should create_class('timezone') }.to raise_error(Puppet::Error, /is not a boolean/) }
+      it { expect { is_expected.to create_class('timezone') }.to raise_error(Puppet::Error, /expects a Boolean value/) }
     end
   end
 end
